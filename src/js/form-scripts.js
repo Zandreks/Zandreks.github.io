dddd22b3aa3156fc8a5a69c68186b8911d7c8a8b
Maxsim Severyanov <zandreks@gmail.com>
2019-10-18 19:17:54 +0600

    fix

--- src/js/form-scripts.js
+++ src/js/form-scripts.js
@@ -21,7 +21,7 @@ function submitForm() {
 
   $.ajax({
     type: "POST",
-    url: "php/form-process.php",
+    url: "https://emailsubadd.inva.kz/form-process.php",
     data: "name=" + name + "&email=" + email + "&message=" + message,
     success: function(text) {
       if (text == "success") {
