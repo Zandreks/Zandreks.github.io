ca2411f690ad0086a8e33e454d79632ae8b6f4c4
Maxsim Severyanov <zandreks@gmail.com>
2019-10-18 18:53:47 +0600

    emaiil

--- src/js/form-scripts.js
+++ src/js/form-scripts.js
@@ -1,54 +1,80 @@
-$("#contactForm").validator().on("submit", function (event) {
+$("#contactForm")
+  .validator()
+  .on("submit", function(event) {
     if (event.isDefaultPrevented()) {
-        // handle the invalid form...
-        formError();
-        submitMSG(false, "Did you fill in the form properly?");
+      // handle the invalid form...
+      formError();
+      submitMSG(false, "Вы правильно заполнили форму?");
     } else {
-        // everything looks good!
-        event.preventDefault();
-        submitForm();
+      // everything looks good!
+      event.preventDefault();
+      submitForm();
     }
-});
+  });
 
-
-function submitForm(){
-    //$( "#form-submit" ).prop( "disabled", true );
-    // Initiate Variables With Form Content
-    var name = $("#name").val();
-    var email = $("#email").val();
-    var message = $("#message").val();
-
-    $.ajax({
-        type: "POST",
-        url: "php/form-process.php",
-        data: "name=" + name + "&email=" + email + "&message=" + message,
-        success : function(text){
-            if (text == "success"){
-                formSuccess();
-            } else {
-                formError();
-                submitMSG(false,text);
-            }
+function submitForm() {
+  //$( "#form-submit" ).prop( "disabled", true );
+  // Initiate Variables With Form Content
+  var name = $("#name").val();
+  var email = $("#email").val();
+  var message = $("#message").val();
+  var data = {
+    email: {
+      text: message,
+      subject: "Новое письмо",
+      from: {
+        name: name,
+        email: email
+      },
+      to: [
+        {
+          name: "Maxim Severyanov",
+          email: "zandreks@gmail.com"
         }
-    });
-}
-
-function formSuccess(){
-    $("#contactForm")[0].reset();
-    submitMSG(true, "Message Submitted!")
-}
-
-function formError(){
-    $("#contactForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-        $(this).removeClass();
-    });
-}
-
-function submitMSG(valid, msg){
-    if(valid){
-        var msgClasses = "h3 text-center tada animated text-success";
-    } else {
-        var msgClasses = "h3 text-center text-danger";
+      ]
     }
-    $("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
+  };
+
+  $.ajax({
+    type: "POST",
+    url: "https://api.sendpulse.com/smtp/emails",
+    data: JSON.stringify(data),
+    success: function(text) {
+      if (text == "success") {
+        formSuccess();
+      } else {
+        formError();
+        submitMSG(false, text);
+      }
+    }
+  });
+}
+
+function formSuccess() {
+  $("#contactForm")[0].reset();
+  submitMSG(true, "");
+}
+
+function formError() {
+  $("#contactForm")
+    .removeClass()
+    .addClass("shake animated")
+    .one(
+      "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
+      function() {
+        $(this).removeClass();
+      }
+    );
+}
+
+function submitMSG(valid, msg) {
+  if (valid) {
+    var msgClasses = "h3 text-center tada animated text-success";
+  } else {
+    var msgClasses = "h3 text-center text-danger";
+  }
+  $("#msgSubmit")
+    .removeClass()
+    .addClass(msgClasses)
+    .text(msg);
 }
